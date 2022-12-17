5bbca11454eb0c32280f1cdd548c61df5066c4d3
bluewhale507 <jec9663@gmail.com>
2022-12-17 19:01:16 +0900

    [sec11_basicAPI:ObjectClass]

---
+++ sec11_basicAPI/equals/Member.java
@@ -0,0 +1,22 @@
+package sec11_basicAPI.equals;
+
+public class Member {
+    public String id;
+
+    public Member(String id) {
+        this.id = id;
+    }
+
+    //최상위 클래스 Object의 equals method를 Override
+    //String Class의 equals method로 id의 문자열을 비교
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Member) {
+            Member member = (Member) obj;
+            if (id.equals(member.id)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ sec11_basicAPI/hashCode/Key.java
@@ -0,0 +1,26 @@
+package sec11_basicAPI.hashCode;
+
+public class Key {
+    public int number;
+
+    public Key(int number) {
+        this.number = number;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(obj instanceof Key) {
+            Key compareKey = (Key)obj;
+            if(this.number == compareKey.number) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    //컬렉션을 비교하는 경우 equals 결과뿐 아니라 hash값도 동등해야 한다.
+    @Override
+    public int hashCode() {
+        return number;
+    }
+}
