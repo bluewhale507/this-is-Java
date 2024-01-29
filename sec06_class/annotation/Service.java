3b5374f3b0d6025de87f7c6a37cdff2f63ac4952
bluewhale507 <jec9663@gmail.com>
2024-01-29 15:10:01 +0900

    [sec06_class]

---
+++ sec06_class/annotation/Service.java
@@ -0,0 +1,19 @@
+package sec06_class.annotation;
+
+public class Service {
+    @PrintAnnotation
+    public void method1() {
+        System.out.println("실행내용 1");
+    }
+
+    @PrintAnnotation("*")
+    public void method2() {
+        System.out.println("실행내용 2");
+    }
+
+    @PrintAnnotation(value="#", number=20)
+    public void method3() {
+        System.out.println();
+    }
+
+}
