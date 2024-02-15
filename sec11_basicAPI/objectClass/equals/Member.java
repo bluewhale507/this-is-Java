427e8b7c481de0143cf83931906b73f5e3e4770f
bluewhale507 <jec9663@gmail.com>
2024-02-15 21:51:52 +0900

    주석 수정

--- sec11_basicAPI/objectClass/equals/Member.java
+++ sec11_basicAPI/objectClass/equals/Member.java
@@ -12,6 +12,8 @@ public class Member {
     @Override
     public boolean equals(Object obj) {
         if (obj instanceof Member) {
+//        	Object로 promotion된 인스턴스는 Object가 가진 필드와 메서드(재정의한경우에는 재정의한 메서드)만 
+//        	사용가능 하므로, 객체비교시 사용할 id 필드가 Object 클래스에는 없어서, 다시 Member 타입으로 Casting 해줘야 함.
             Member member = (Member) obj;
             if (id.equals(member.id)) {
                 return true;
