d878797786e769b9b044d6bdaf3d9a0ef804a355
bluewhale507 <jec9663@gmail.com>
2024-03-10 07:18:26 +0900

    [sec12_Thread_synchronized]

---
+++ sec12_multiThread/synchronizedExample/User1.java
@@ -0,0 +1,14 @@
+package sec12_multiThread.synchronizedExample;
+
+public class User1 extends Thread {
+	 private Calculator calculator;
+	 
+	 public void setCalculator(Calculator calculator) {
+		 this.setName("User1");
+		 this.calculator = calculator;
+	 }
+	 
+	 public void run() {
+		 calculator.setMemory(100);
+	 }
+}
